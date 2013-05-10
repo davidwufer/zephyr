a4354c7b368a0df9ce2dcb8cabd87fbb454d720c
David Wu <davidcnwu@gmail.com>
2013-05-09 22:32:48 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zephyr::Application.config.secret_token = 'fee020f1abd646fe155d964d713dbde7763a1cb116e43f08ac55ff5a8d39dd3f9297ca2c91d6603fecbb251273a87a07d3bbd2e8e1749e5b881254395a5976ae'
